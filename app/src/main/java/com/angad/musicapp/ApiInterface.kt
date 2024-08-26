b55292bf95153213531019ab2f275c87d9dd0ca0
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-08-27 02:55:17 +0530

    step4: Fetch the data in main activity

--- app/src/main/java/com/angad/musicapp/ApiInterface.kt
+++ app/src/main/java/com/angad/musicapp/ApiInterface.kt
@@ -10,5 +10,5 @@ interface ApiInterface {
     @Headers("x-rapidapi-key: 92b8a40cd3mshe77777a773fba08p1205a0jsnd9b2262395e0",
       "x-rapidapi-host: deezerdevs-deezer.p.rapidapi.com")
     @GET("search")
-    fun getData(@Query("q") query: String) : Call<List<MyData>>
+    fun getData(@Query("q") query: String) : Call<MyData>
 }
--- app/src/main/java/com/angad/musicapp/MainActivity.kt
+++ app/src/main/java/com/angad/musicapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.angad.musicapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -25,6 +30,24 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData("eminem")
+
+        retrofitData.enqueue(object : Callback<MyData?> {
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+
+//              if the API call is success then this method is executed
+                val dataList = response.body()?.data
+                val textView = findViewById<TextView>(R.id.helloText)
+                textView.text = dataList.toString()
+                Log.d("onResponse","onResponse: " + response.body())
+            }
+
+            override fun onFailure(p0: Call<MyData?>, p1: Throwable) {
+//              if the API call is failure then this method is executed
+                Log.d("Tag: onFailure","onFailure: " + p1.message)
+            }
+        })
 
     }
 }
